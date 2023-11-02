f0ab363f5b8087c19af4f2b674de0c8173a1f947
= <=>
2023-11-02 20:56:52 +0530

    kkkk

--- client/src/index.js
+++ client/src/index.js
@@ -12,8 +12,6 @@ const store = createStore(Reducers, compose(applyMiddleware(thunk)));
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-   
       <App />
-   
   </Provider>
 );
